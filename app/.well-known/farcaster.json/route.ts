7d73974a6f3347d1d0353e2afb7ddea7b465edea
muhamad rivai <muhamadrivai193@gmail.com>
2025-11-07 17:18:31 +0700

    updated

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -8,9 +8,9 @@ export async function GET() {
 const URL = process.env.NEXT_PUBLIC_URL as string;
 return Response.json({
   "accountAssociation": {  // these will be added in step 5
-    "header": "",
-    "payload": "",
-    "signature": ""
+    "header": "eyJmaWQiOjE0NTAxMjgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg0QjNjM2MyQWVCMzczOGRiQzNCODkzZmI1QmVBMkNEMUY2YzUwZUY0In0",
+    "payload": "eyJkb21haW4iOiJudXdhLWJhc2VhcHAudmVyY2VsLmFwcCJ9",
+    "signature": "bjiG2v/PIQYxw/HJE5VygyKPD0QwOoto0q4sEHNfKloNaONupx2sxnJA2lRTdAY3/wZDvFygrM7+1tFYRk+E3xw="
   },
   "baseBuilder": {
     "ownerAddress": "0x" // add your Base Account address here
